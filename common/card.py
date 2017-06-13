f8bf9acad3cfa2a513f85c4beb674d0ef5f05d23
Ubuntu <oscar_blazejewski@yahoo.fr>
2017-06-13 12:31:41 -0700

    memelord

---
+++ common/card.py
@@ -0,0 +1,63 @@
+#!/home/oscar/anaconda3/bin/python3
+import logging
+
+class Card:
+
+
+	def __init__(self,card=0):
+		self.card = 0
+		self.rank = 0
+		self.suit = 0
+
+		if card != 0:
+			try:
+				assert type(card) == str
+				assert len(card) == 2
+				assert self.isValidCard(card[0]) == True 
+				assert self.isValidSuit(card[1]) == True
+				self.card = card
+				self.setValues()
+				
+			except AssertionError:
+				logging.warning('error in Card format for variable: '+str(card))
+					
+
+	def isValidCard(self,char):
+		if char in ['A','K','Q','J','T','9','8','7','6','5','4','3','2']:
+			return True
+		else:
+			return False
+	
+	def isValidSuit(self,char):
+		if char in ['s','h','d','c']:
+			return True
+		else:
+			return False
+	
+	def setValues(self):
+		try:
+			self.rank = int(self.card[0])
+		except:
+			if self.card[0] == 'A':
+				self.rank = 14
+			elif self.card[0] == 'K':
+				self.rank = 13
+			elif self.card[0] == 'Q':
+				self.rank = 12
+			elif self.card[0] == 'J':
+				self.rank = 11
+			elif self.card[0] == 'T':
+				self.rank = 10
+
+		if self.card[1] == 's':
+			self.suit = 1
+		elif self.card[1] == 'h':
+			self.suit = 2
+		elif self.card[1] == 'c':
+			self.suit = 3
+		elif self.card[1] == 'd':
+			self.suit = 4
+
+
+c = Card('8s')
+print(c.rank,c.suit)
