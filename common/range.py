bbb47594642da0121c2691112035f8dcf94000a6
Ubuntu <oscar_blazejewski@yahoo.fr>
2017-06-15 13:19:16 -0700

    cleaned range print

--- common/range.py
+++ common/range.py
@@ -8,12 +8,11 @@ class Range:
 		self.pairs = []
 		self.offsuit = {}
 		self.suited = {}
-		self.parseRange(rangeStr)
+		self.addRange(rangeStr)
 		
 	#gets pairs, offsuited and suited from like ATs+, 99+, AJs+
-	def parseRange(self, rangeStr):
+	def addRange(self, rangeStr):
 		rangeStr = rangeStr.replace(' ','').split(',')
-		print(rangeStr)
 		for expression in rangeStr:
 			if len(expression) == 2: 
 				if expression[0] == expression[1]:
@@ -26,12 +25,14 @@ class Range:
 				elif expression[2] == 's':
 					if expression[0] not in self.suited:
 						self.suited[expression[0]] = []
-					self.suited[expression[0]].append(expression[1])
+					if expression[1] not in self.suited[expression[0]]:
+						self.suited[expression[0]].append(expression[1])
 
 				elif expression[2] == 'o':
 					if expression[0] not in self.offsuit:
 						self.offsuit[expression[0]] = []
-					self.offsuit[expression[0]].append(expression[1])
+					if expression[1] not in self.offsuit[expression[0]]:
+						self.offsuit[expression[0]].append(expression[1])
 
 			elif len(expression) == 4:
 				if expression[2:] == 's+':
@@ -44,7 +45,7 @@ class Range:
 	def setPairs(self,expression):
 		try:
 			treshhold = int(expression[0])
-			for i in range(treshhold,9):
+			for i in range(treshhold,10):
 				self.pairs.append(str(i))
 			self.pairs += ['T','J','Q','K','A']
 
@@ -62,19 +63,39 @@ class Range:
 
 	#AT+ 
 	def setSuited(self,expression):
+		if expression[0] == 'A':
+			if 'A' not in self.suited:
+				self.suited['A'] = []
+			for j in range(Card.getValue(expression[1]),14):
+				if Card.getStr(j) not in self.suited['A']:
+					self.suited['A'].append(Card.getStr(j))
+				
+		else:
 
-		for i in range(Card.getValue(expression[0]),15):
-			if Card.getStr(i) not in self.suited:
-				self.suited[Card.getStr(i)] = []
+			for i in range(Card.getValue(expression[0]),15):
+				if Card.getStr(i) not in self.suited:
+					self.suited[Card.getStr(i)] = []
 
-			self.suited[Card.getStr(i)].append(Card.getStr(Card.getValue(expression[1])+(i-Card.getValue(expression[0]))))
+				if (Card.getStr(Card.getValue(expression[1])+(i-Card.getValue(expression[0])))) not in self.suited[Card.getStr(i)]:
+					self.suited[Card.getStr(i)].append(Card.getStr(Card.getValue(expression[1])+(i-Card.getValue(expression[0]))))
+
 
 	def setOffsuit(self,expression):
-		for i in range(Card.getValue(expression[0]),15):
-			if Card.getStr(i) not in self.offsuit:
-				self.offsuit[Card.getStr(i)] = []
+		if expression[0] == 'A':
+			if 'A' not in self.offsuit:
+				self.offsuit['A'] = []
+			for j in range(Card.getValue(expression[1]),14):
+				if Card.getStr(j) not in self.offsuit['A']:
+					self.offsuit['A'].append(Card.getStr(j))
+				
+		else:
 
-			self.offsuit[Card.getStr(i)].append(Card.getStr(Card.getValue(expression[1])+(i-Card.getValue(expression[0]))))
+			for i in range(Card.getValue(expression[0]),15):
+				if Card.getStr(i) not in self.offsuit:
+					self.offsuit[Card.getStr(i)] = []
+
+				if (Card.getStr(Card.getValue(expression[1])+(i-Card.getValue(expression[0])))) not in self.offsuit[Card.getStr(i)]:
+					self.offsuit[Card.getStr(i)].append(Card.getStr(Card.getValue(expression[1])+(i-Card.getValue(expression[0]))))
 		
 	def print(self):
 		string = ''
@@ -85,8 +106,7 @@ class Range:
 						string += Card.getStr(i)+Card.getStr(i)+'  '
 					else:
 						string += '    '
-				else:
-				#TO CONTINUE WITH i>J and J<I
+				elif j < i: #suited 
 					if Card.getStr(i) in self.suited:
 						if Card.getStr(j) in self.suited[Card.getStr(i)]:
 							string += Card.getStr(i)+Card.getStr(j)+'s ' 
@@ -95,22 +115,42 @@ class Range:
 					else:
 						string += '    '
 				else:
-					if Card.getStr(i) in self.offsuit:
-						if Card.getStr(j) in self.offsuit[Card.getStr(i)]:
-							string += Card.getStr(i)+Card.getStr(j)+'o ' 
+					if Card.getStr(j) in self.offsuit:
+						if Card.getStr(i) in self.offsuit[Card.getStr(j)]:
+							string += Card.getStr(j)+Card.getStr(i)+'o ' 
 						else:
 							string += '    '
 					else:
 						string += '    '
 			string +='\n'			
 		print(string)
+	
+	#MEMO 2652 combos
+	# XX 6 combos ( 3 if 1 blocker)
+	# XYo 12 combos
+	# XYs 4 combos
+	# CURRENTLY RETTURNING 1/2x pokerstoves percentage
+	def getRangePercentage(self):
 
+		combos = 6*len(self.pairs)
+		nb= 0
+		for i in self.suited:
+			nb+= len(self.suited[i])
 
+		combos += 4*nb
+
+		nb = 0
+
+		for i in self.offsuit:
+			nb+= len(self.offsuit[i])
+		combos += 12*nb
+
+		print(combos)
+		return combos/26.52
 
 				
 			
-r = Range('22,88,43o,34s')	
-print('pairs:',r.pairs)	
-print('suited:',r.suited)
-print('offsuit',r.offsuit)
-r.print()
+#r = Range('22+,AQs+,AKo')
+#r.addRange('A2s+')
+#r.print()
+#print('Range:',r.getRangePercentage())
