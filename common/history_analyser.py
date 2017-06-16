a2347a09fdfe9b1fd6aae3b4bc06cad642c49b4f
Oscar Blazejewski <oscar_blazejewski@yahoo.fr>
2017-06-16 11:35:08 +0200

    Added history analyser

---
+++ common/history_analyser.py
@@ -0,0 +1,24 @@
+#!/home/oscar/anaconda3/bin/python3
+
+#can filter hands by hero position, actions, range,...
+class HistoryAnalyser:
+
+	def __init__(self,filename):
+		self.data = 0
+		self.filtered_data = self.data
+	
+	def setHero(self,heroName):
+		pass
+
+	def setFilter(self, filters):
+		pass
+	def addFilter(self,filters):
+		pass
+	def removeFilter(self,filters):
+		pass
+	def filterHands(self):
+		pass
+	def getPlayerStats(self,playerName):
+		pass
+	def getHeroStats(self):
+		pass
