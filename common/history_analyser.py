dd36990bf6ff21327f4bfea8531df13dd9215113
Oscar Blazejewski <oscar_blazejewski@yahoo.fr>
2017-06-16 17:10:08 +0200

    Update

--- common/history_analyser.py
+++ common/history_analyser.py
@@ -1,4 +1,5 @@
 #!/home/oscar/anaconda3/bin/python3
+import logging
 
 #can filter hands by hero position, actions, range,...
 class HistoryAnalyser:
@@ -6,16 +7,44 @@ class HistoryAnalyser:
 	def __init__(self,filename):
 		self.data = 0
 		self.filtered_data = self.data
+		self.filters = {}
+		self.filters['position'] = []
 	
 	def setHero(self,heroName):
-		pass
+		self.hero = heroName
 
-	def setFilter(self, filters):
-		pass
+	# example: 'position CU BU CO', 'position CU'
 	def addFilter(self,filters):
-		pass
+		parsed = filters.split(',')
+		for filterx in parsed:
+			operands = filterx.split(' ')
+			if operands[0] == 'position':
+				for i in range(1,len(operands)):
+					if operands[i] == 'EP':
+						if 4 not in self.filters['position']:
+							self.filters['position'].append(4)
+					elif operands[i] == 'MP':
+						if 5 not in self.filters['position']:
+							self.filters['position'].append(5)
+					elif operands[i] == 'CU':
+						if 6 not in self.filters['position']:
+							self.filters['position'].append(6)
+					elif operands[i] == 'BU':
+						if 1 not in self.filters['position']:
+							self.filters['position'].append(1)
+					elif operands[i] == 'SB':
+						if 2 not in self.filters['position']:
+							self.filters['position'].append(2)
+					elif operands[i] == 'BB':
+						if 3 not in self.filters['position']:
+							self.filters['position'].append(3)
+			else:
+				logging.warning('Unknown filter for '+filterx)
+					
+			
 	def removeFilter(self,filters):
 		pass
+
 	def filterHands(self):
 		pass
 	def getPlayerStats(self,playerName):
