ea5c0117ee089be4c2d509cc792518147f7af717
Xavier Hermand <xavier.hermand@laposte.net>
2017-06-20 04:53:18 -0700

    Added json parsing and position filtering in history analyser

--- common/history_analyser.py
+++ common/history_analyser.py
@@ -1,14 +1,22 @@
 #!/home/oscar/anaconda3/bin/python3
+
 import logging
+import json
 
 #can filter hands by hero position, actions, range,...
 class HistoryAnalyser:
 
 	def __init__(self,filename):
-		self.data = 0
-		self.filtered_data = self.data
+		try:
+			fjsondump = open(filename)
+		except:	
+			logging.critical('Can\'t open '+filename+'. Bye Bye !')
+			quit()
+		self.data = json.loads(fjsondump.read())
+		self.filtered_data = []
 		self.filters = {}
 		self.filters['position'] = []
+		
 	
 	def setHero(self,heroName):
 		self.hero = heroName
@@ -45,9 +53,28 @@ class HistoryAnalyser:
 	def removeFilter(self,filters):
 		pass
 
-	def filterHands(self):
-		pass
+	def filterHands(self):	
+		for hand in self.data:
+			isFiltrate = True			
+			if 'players' in hand:				
+				
+				# position	
+				if self.filters['position']:					
+					for player in hand['players']:
+						if player['name'] == self.hero and player['seat'] not in self.filters['position']:
+							isFiltrate = False
+												
+			if isFiltrate:
+				self.filtered_data.append(hand)
+
 	def getPlayerStats(self,playerName):
 		pass
 	def getHeroStats(self):
 		pass
+
+if __name__ == '__main__':
+	HA = HistoryAnalyser('dump.json')
+	HA.setHero('daftReivaX')
+	HA.addFilter('position SB BB')
+	HA.filterHands()
+	#print(HA.filtered_data)
